b1a71605a954310d1c296efb66ceecf0e3e3e196
griffinmacias <griffinmacias@gmail.com>
2018-09-08 08:17:06 -0400

    Adding xcode project

---
+++ hours/hours/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hours
+//
+//  Created by Mason Macias on 9/8/18.
+//  Copyright © 2018 Mason Macias. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
